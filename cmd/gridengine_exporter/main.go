bfe19944414ccfb4a5d66d1eee9e869c96518933
Jean Pijon <3503714+jeanpijon@users.noreply.github.com>
2023-02-03 15:28:00 +0100

    Fix/pending jobs invisible if q specified (#4)
    * Removed filter paramter.
    When using load_values as a requestable paramaeter in job (e.g. -l mem_free=2G). It will not be shown in case the -q param is specified in qstat.
    
    This is due to the way how load_values are used. Scheduler will assign the queue at the moment of start of the job, as there is no way for scheduler to know, how the load_value will behave in the future. (kinda like Schroedinger job...)
    
    ---------
    
    Co-authored-by: Jan Pesan <janp@speechmatics.com>

--- cmd/gridengine_exporter/main.go
+++ cmd/gridengine_exporter/main.go
@@ -11,13 +11,11 @@ import (
 )
 
 var addr = flag.String("listen-address", ":8080", "The address to listen on for HTTP requests.")
-var filter = flag.String("host-filter", "*", "Host to filter for, if not wildcard, only metrics for specified host is collected.")
 
 func main() {
 	flag.Parse()
 
 	gridengineCollector := collector.NewGridengineCollector()
-	gridengineCollector.Filter = *filter
 	prometheus.MustRegister(gridengineCollector)
 
 	http.Handle("/metrics", promhttp.Handler())
